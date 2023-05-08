9008bb70a0b4273f4668bd844282cf50f0ac1c1a
Luccas Freitas <luccas.matheus@academico.ufpb.br>
2023-05-08 14:13:31 -0300

    Testes unitários e readme

---
+++ src/__tests__/ListaSequencialUnit.test.ts
@@ -0,0 +1,117 @@
+import {ListaSequencial} from  "../core/ListaSequencial";
+
+// Tamanho máximo da lista
+const TAM_MAX = 8; 
+
+// Utilizada inicializar com valores pré-definidos
+const defaultList = [1, 2, 3, 4, 5, 6, 7, 8]
+
+// Valores
+const VALOR_ZERO = 0;
+const VALOR_UM = 1;
+const VALOR_DOIS = 2;
+const VALOR_OITO = 8;
+const VALOR_NOVE = 9;
+
+// Posições
+const POSICAO_ZERO = 0;
+const POSICAO_UM = 1;
+const POSICAO_DOIS = 2;
+const POSICAO_OITO = 8;
+const POSICAO_NOVE = 9;
+
+// Descrição dos testes
+const SEM_RETORNO = -1; // Não deve retornar nada
+const NAO_ENCONTRADO = -1; // Não deve encontrar
+
+describe("Testes Unitários da Lista Sequencial", () => {
+
+    test("Um lista deve ser instanciada corretamente", () => {
+        
+        const list = new ListaSequencial(8, []);
+
+        // Lista deve estar vazia
+        expect(list.listIsEmpty()).toBe(true); 
+        // Lista deve ter tamanho 0 (vazia)
+        expect(list.size()).toBe(0); 
+        // Lista não pode estar cheia
+        expect(list.listIsfull()).toBe(false); 
+    });
+    test("A lista default deve estar correta", () => {
+        
+        const list = new ListaSequencial(TAM_MAX, defaultList);
+        
+        // Lista não deve estar vazia
+        expect(list.listIsEmpty()).toBe(false); 
+        // Lista deve ter tamanho 8
+        expect(list.size()).toBe(TAM_MAX); 
+        // Lista deve estar cheia
+        expect(list.listIsfull()).toBe(true); 
+        
+    });
+
+    test("Busca por valor e posição deve funcionar corretamente", () => {
+        const list = new ListaSequencial(8, defaultList);
+
+        // Por posição
+        expect(list.searchInList(undefined, POSICAO_UM)).toBe(VALOR_UM); // Com posição deve encontrar o valor 1 na pos 1
+        expect(list.searchInList(undefined, POSICAO_OITO)).toBe(VALOR_OITO); // Com posição deve encontrar o valor 8 na pos 8
+        expect(list.searchInList(undefined, POSICAO_NOVE)).toBe(SEM_RETORNO); // Na posição 9 não deve ter retorno
+        expect(list.searchInList(undefined, POSICAO_ZERO)).toBe(SEM_RETORNO); // Na posição 0 não deve ter retorno
+
+        // Por valor
+        expect(list.searchInList(VALOR_OITO)).toBe(POSICAO_OITO); // Por valor deve encontar o 1 na posição 1
+        expect(list.searchInList(VALOR_NOVE)).toBe(SEM_RETORNO); // Com número não deve encontar o valor 9
+        
+        // Tratamento de erro
+        expect(list.searchInList(undefined, undefined)).toBe(SEM_RETORNO); // Valor ou posição deve ser informado
+        expect(list.searchInList(VALOR_ZERO, POSICAO_UM)).toBe(SEM_RETORNO); // Valor OU posição deve ser informado, nunca os dois
+    });
+
+    test("Remover um elemento da lista deve funcionar corretamente", () => {
+        const list = new ListaSequencial(8, defaultList);
+        
+        list.removeInList(VALOR_UM); // Remove o número 1 da lista
+        expect(list.searchInList(VALOR_UM)).toBe(NAO_ENCONTRADO); // A busca com número não deve encontrar o valor 1
+
+        // Se a lista está se movendo corretamente, o número 2 deve estar na posição 1, e a pos 8 deve ser undefined
+        expect(list.searchInList(undefined, POSICAO_UM)).toBe(VALOR_DOIS); // A busca com posição deve encontrar o valor 2 na pos 1
+        expect(list.searchInList(undefined, POSICAO_OITO)).toBe(undefined); // A busca com posição deve retornar undefined na pos 8
+    });
+
+    test("Inserir um elemento na lista deve funcionar corretamente", () => {
+        const incompleteList = [2, 3, 4, 5, 6, 7, 8];
+        const list = new ListaSequencial(8, incompleteList);
+
+        //Pre-check na lista incompleta
+        list.searchInList(undefined, POSICAO_OITO); 
+        list.searchInList(undefined, POSICAO_UM).toBe(VALOR_DOIS); 
+        list.size().toBe(7);
+
+        // Inserindo o número 1 na lista
+        list.addInList(VALOR_UM, POSICAO_UM).toBe(true);
+        list.size().toBe(8); // A lista deve ter tamanho 8 agora
+        list.listIsfull().toBe(true); // A lista deve estar cheia agora
+
+        // O valor 1 deve ser encontrado na posição 1 em ambas as buscas
+        expect(list.searchInList(undefined, POSICAO_UM)).toBe(VALOR_UM);
+        expect(list.searchInList(VALOR_UM, undefined)).toBe(POSICAO_UM);
+
+        // Se estiver movendo corretamente, o valor 2 deve estar na posição 2 e o 8 na posicao 8
+        expect(list.searchInList(undefined, POSICAO_DOIS)).toBe(VALOR_DOIS);
+        expect(list.searchInList(undefined, POSICAO_OITO)).toBe(VALOR_OITO);
+
+        // Não deve ser possível inserir um valor em uma posição fora do range
+        list.addInList(VALOR_NOVE, POSICAO_NOVE).toBe(SEM_RETORNO);
+        list.addInList(VALOR_NOVE, POSICAO_ZERO).toBe(SEM_RETORNO);
+
+        // Não deve ser possível inserir em posicao > size + 1
+        const emptyList = new ListaSequencial(8, []);
+        emptyList.addInList(VALOR_UM, POSICAO_DOIS).toBe(SEM_RETORNO);
+        emptyList.addInList(VALOR_UM, POSICAO_OITO).toBe(SEM_RETORNO);
+
+    });
+        
+});
+
+   
